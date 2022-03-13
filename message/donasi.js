308a8f5f8d0e1fd12329f06e462b5e8abea522b3
BRANZZGEY <bukanakundanzz@gmail.com>
2022-03-13 19:14:03 +0700

    BranzzBotz

---
+++ message/donasi.js
@@ -0,0 +1,28 @@
+exports.dodo = (prefix) => {
+return`
+*╭─▸*「 *DONASI* 」
+*┴*
+*│* Bantu Developer ngembangin bot ini
+*│* dengan donasi seiklasnya 🙂
+*│*
+*│* Shopee : 085335877178
+*│* Gopay : 085335877178
+*│* Dana : 085335877178
+*│* Ovo : 085335877178
+*│*
+*│* Makasih yang udah donasi
+*│* dan bantu ngembangin bot ini
+*│* moga rejekinya lancar amin
+*┬*
+*╰────────────[*`
+}
+
+
+
+
+
+
+
+
+
+
